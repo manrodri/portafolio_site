4f2739595cab733b457c10669517f36c6ade82c9
Ubuntu <ubuntu@ip-172-31-39-16.eu-west-1.compute.internal>
2020-01-17 07:59:40 +0000

    retaken work for portafolio site

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-$(document).ready(function () {
-
+$(document).ready(function (){
+    $('.menu-toogler').on('click', function(){
+        $(this).toggleClass('open');
+        $('.top-nav').toggleClass('open');
+     
+    });
 });
